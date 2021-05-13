482167455793044b470d11fb873f0e05593a5a51
ThureinTun <you@example.com>
2021-05-13 13:20:45 +0630

    create Spring Project

---
+++ src/main/java/com/thurein/MySpringShop/MySpringShopApplication.java
@@ -0,0 +1,13 @@
+package com.thurein.MySpringShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thurein/MySpringShop/MySpringShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thurein.MySpringShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
